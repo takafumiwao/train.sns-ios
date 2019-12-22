e60286efa42daffc8cc4c6bc54132640c8bab22e
dazy <csl.nodajima@gmail.com>
2019-12-22 21:38:05 +0900

    Initial Commit

---
+++ TRAIN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TRAIN
+//
+//  Created by Naoki Odajima on 2019/12/22.
+//  Copyright © 2019 Naoki Odajima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
