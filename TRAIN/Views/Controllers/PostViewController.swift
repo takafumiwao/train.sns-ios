12ea598f1fcecc78d700c620d25928d6d1606a3f
TAKAFUMI IWAO <koshiiwao2904@icloud.com>
2020-02-24 21:11:27 +0900

    #12 投稿画面に遷移するボタンの作成

---
+++ TRAIN/Views/Controllers/PostViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PostViewController.swift
+//  TRAIN
+//
+//  Created by 岩男高史 on 2020/02/24.
+//  Copyright © 2020 Naoki Odajima. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- TRAIN/Views/Controllers/TabBarController.swift
+++ TRAIN/Views/Controllers/TabBarController.swift
@@ -6,22 +6,55 @@
 //  Copyright © 2019 Naoki Odajima. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class TabBarController: UITabBarController {
+    private let disposeBag = DisposeBag()
+    // 投稿画面に遷移するボタン
+    private let postButton = UIButton()
+    // safeArea
+    var topSafeAreaHeight: CGFloat = 0
+    var bottomSafeAreaHeight: CGFloat = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // ボタンタップされたら投稿画面へ遷移
+        postButton.rx.tap.subscribe(onNext: { [unowned self] _ in
+            self.goToPostView()
+        }).disposed(by: disposeBag)
     }
 
-    /*
-     // MARK: - Navigation
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        // safeArea取得
+        topSafeAreaHeight = view.safeAreaInsets.top
+        bottomSafeAreaHeight = view.safeAreaInsets.bottom
+        // postButtonの設定
+        postButton.frame.size = CGSize(width: view.frame.width / 5, height: view.frame.width / 5)
+        postButton.frame.origin.y = tabBar.frame.origin.y - (tabBar.frame.height - bottomSafeAreaHeight) / 1.5
+        postButton.center.x = view.frame.width / 2
+        postButton.setImage(UIImage(systemName: "camera"), for: .normal)
+        postButton.layer.cornerRadius = postButton.frame.size.width / 2
+        postButton.backgroundColor = UIColor.white
+        postButton.layer.shadowOpacity = 0.2
+        postButton.layer.shadowRadius = postButton.frame.size.width / 2
+        postButton.layer.shadowColor = UIColor.black.cgColor
+        postButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        view.addSubview(postButton)
+        // tabBarアイテムの位置を変更する
+        tabBar.itemPositioning = .centered
+        tabBar.itemSpacing = view.frame.width / 3.1
+        tabBar.itemWidth = view.frame.width / 3
+    }
 
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
+    func goToPostView() {
+        // 画面遷移処理
+        let storyBoard = UIStoryboard(name: "PostViewController", bundle: nil)
+        let nxViewController = storyBoard.instantiateViewController(withIdentifier: "PostViewController") as! PostViewController
+        let nav = UINavigationController(rootViewController: nxViewController)
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true, completion: nil)
+    }
 }
