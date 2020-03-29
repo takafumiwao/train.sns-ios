e8044734512b6fe9b4cb3e3d7c43f441a0783727
TAKAFUMI IWAO <koshiiwao2904@icloud.com>
2020-03-29 21:15:00 +0900

    #13 タグ追加画面作成

---
+++ TRAIN/Views/Controllers/TagViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TagViewController.swift
+//  TRAIN
+//
+//  Created by 岩男高史 on 2020/03/24.
+//  Copyright © 2020 Naoki Odajima. All rights reserved.
+//
+
+import UIKit
+
+class TagViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
+}
